396cdf58e955dfbe9d50b0043a93955da03b3d41
psmacabulos <patrick.macabulos@gmail.com>
2025-08-16 22:39:08 +1000

    Migrate ESlint to v9

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <>
       <h1 className="text-5xl font-light text-violet-700">Hello Patrick</h1>;
-      <h2 className="font-space-grotesk text-5xl  font-light text-violet-700">Hello Patrick</h2>
     </>
   );
 }
